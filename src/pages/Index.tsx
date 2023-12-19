1a036ae4571bc809a7d0702eb3fa6828c1351604
TaranIgor25 <ganj@gmail.com>
2023-12-19 14:10:54 +0300

    add css file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import '../styles/form.css'
 
-const data = {
+  const data = {
   firstName: "",
   lastName: "",
   phone: "",
-  email: "",
+  email: "", 
   gender: "",
   message: "",
 };
@@ -56,72 +57,33 @@ export const Index = () => {
 
   document.body.style.backgroundColor = "#FF9D70";
 
-  const inputStyle = {
-    display: "flex",
-    width: "calc(100% - 14px)",
-
-    margin: "5px 0px",
-    padding: "7px",
-    border: "0px",
-    borderRadius: "20px",
-    backgroundColor: "#E9EFF7",
-  };
-
-  const labelStyle = {
-    width: "100%",
-    marginBottom: "10px",
-  };
-
   return (
-    <div
-      style={{
-        display: "flex",
-        margin: "0 auto",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "325px",
-
-        borderRadius: "30px",
-        backgroundColor: "white",
-      }}
-    >
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-
-          width: "70%",
-          margin: "10%",
-        }}
-      >
-        <label htmlFor="firstName" style={labelStyle}>
+    <div className="div-wrapper">
+      <form className="form"
+        onSubmit={handleSubmit}>
+        <label className='form-label' htmlFor="firstName">
           First Name:
-          <input
+          <input className="form-input"
             required
-            style={inputStyle}
             type="text"
             id="firstName"
             value={formData.firstName}
             onChange={handleChange}
           ></input>
         </label>
-        <label htmlFor="lastName" style={labelStyle}>
+        <label className='form-label' htmlFor="lastName">
           Last Name:
-          <input
-            style={inputStyle}
+          <input className="form-input"
             type="text"
             id="lastName"
             value={formData.lastName}
             onChange={handleChange}
           ></input>
         </label>
-        <label htmlFor="phone" style={labelStyle}>
+        <label className='form-label' htmlFor="phone">
           Phone:
-          <input
+          <input className="form-input"
             required
-            style={inputStyle}
             placeholder="+375ххххххххх"
             type="tel"
             id="phone"
@@ -129,77 +91,44 @@ export const Index = () => {
             onChange={handleChange}
           ></input>
         </label>
-        <label htmlFor="email" style={labelStyle}>
+        <label className='form-label' htmlFor="email">
           Email:
-          <input
+          <input className="form-input"
             required
-            style={inputStyle}
             type="email"
             id="email"
             value={formData.email}
             onChange={handleChange}
           ></input>
         </label>
-        <label htmlFor="gender" style={labelStyle}>
+        <label className='form-label' htmlFor="gender">
           {" "}
           Gender:
-          <select
+          <select className="form-select"
             id="gender"
             value={formData.gender}
             onChange={handleChange}
-            style={{
-              width: "100%",
-              padding: "5px",
-
-              borderRadius: "10px",
-              backgroundColor: "#E9EFF7",
-            }}
           >
             <option>Men</option>
             <option>Women</option>
           </select>
         </label>
-        <label htmlFor="message" style={labelStyle}>
+        <label className='form-label' htmlFor="message">
           Comment:
-          <textarea
+          <textarea className="form-area"
             id="message"
             value={formData.message}
             onChange={handleChange}
-            style={{
-              width: "calc(100% - 14px)",
-              maxWidth: "calc(100% - 14px)",
-              padding: "7px",
-
-              backgroundColor: "#E9EFF7",
-            }}
           ></textarea>
         </label>
-        <button
+        <button className="form-btn"
           type="submit"
-          style={{
-            width: "100%",
-            padding: "10px",
-            margin: "5px 0px",
-            border: "0px",
-            borderRadius: "10px",
-
-            color: "white",
-            backgroundColor: "#FF9D70",
-            boxShadow: "0px 3px 0px 0px #E47E5A",
-          }}
         >
           Отправить
         </button>
       </form>
       {dataError && (
-        <div
-          style={{
-            textAlign: "center",
-
-            margin: "0px 15px 30px 15px",
-            color: "red",
-          }}
-        >
+        <div className="form-error">
           {dataError}
         </div>
       )}
